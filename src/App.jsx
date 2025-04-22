605beef94003b7669079d217bab4fbc985de45e9
vnrnesia <102244488+vnrnesia@users.noreply.github.com>
2025-04-22 22:49:37 +0300

    Added functions, components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import ProjectsSidebar from './components/ProjectsSidebar';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
+import { useState } from 'react';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8"> 
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
+    
     </main>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 export default function Input({ props, label, textarea }) {
   const classes =
-    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+    "px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100";
 
   return (
     <p className="flex flex-col gap-1 my-4">
