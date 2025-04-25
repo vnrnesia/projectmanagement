f3b78bc61ccc6e287c80fd0146eeeabf2bdf058b
vnrnesia <102244488+vnrnesia@users.noreply.github.com>
2025-04-25 22:19:06 +0300

    Tasks.jsx created.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,26 @@ function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
+    tasks: [],
   });
+
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskID,
+      };
+
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  function handleDeleteTask() {}
 
   function handleSelectProject(projectId) {
     setProjectsState((prevState) => {
@@ -71,7 +90,15 @@ function App() {
     (project) => project.id === projectsState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+  let content = (
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks.filter}
+    />
+  );
 
   if (projectsState.selectedProjectId === null) {
     content = (
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,26 @@
+import { useState, useRef } from "react";
+import NewTask from "./NewTask.jsx";
+
+export default function Tasks({ tasks, onAdd, onDelete }) {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      <NewTask onAdd={onAdd} />
+      {tasks.length === 0 && (
+        <p className="text-stone-800 my-4">
+          This projecct does not have any tasks
+        </p>
+      )}
+      {tasks.length > 0 && (
+        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+          {tasks.map((task) => (
+            <li key={task.id} className="flex justify-between my-4">
+              <span>{task.text}</span>
+              <button className="text-stone-700 hover:text-red">Clear</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+}
